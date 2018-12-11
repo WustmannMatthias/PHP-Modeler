a76b8bda0709e021ba816f6e94a2ecea45c4d223
WustmannMatthias <m.wustmann@redspher.com>
2018-12-11 14:14:22 +0100

    New script select_iterations.js -> allow to get features to test more ergonomically

--- frames/features.php
+++ frames/features.php
@@ -1,3 +1,5 @@
+<script type="text/javascript" src="style/select_iteration.js"></script>
+
 <?php
 	use GraphAware\Neo4j\Client\ClientBuilder;
 
@@ -24,14 +26,11 @@
 	$query = "MATCH (p:Project) RETURN p.name as project";
 	$result = runQuery($client, $query);
 
-	$projectOptions = "";
+	$projectOptions = "<option value='none' selected>...</option>";
 
 	foreach ($result->records() as $record) {
 		$project = $record->value("project");
 		$projectOptions.= "<option value='$project'";
-		if (isset($_SESSION['project']) && $_SESSION['project'] == $project) {
-			$projectOptions.= " selected";
-		}
 		$projectOptions.= ">$project</option>";
 	}
 ?>
@@ -44,94 +43,65 @@
 	<form class="col-lg-8 col-lg-offset-2 form-horizontal" 
 			method="post" action="index.php?features">
 		
-		<div class="row form-group">
+		<div class="row form-group" id="project_select">
 			<label>Select your project </label>
 			<select name="project">
 				<?php
 					echo $projectOptions;
 				?>
 			</select>
+		</div>
+
+		<div class="row form-group" id="iteration_select">
+			<label>Select the iteration</label>
+			<select name="iteration">
+				<?php
+					//echo $iterationOptions;
+				?>
+			</select>
+		</div>
+
+		<div class="row form-group" id="iteration_submit">
 			<button class="btn btn-primary" type="submit" 
-					name="projectSubmit">Validate
+					name="iterationSubmit">Validate
 			</button>
 		</div>
 
 	</form>
 </div>
 
-<?php
-	if (isset($_POST['projectSubmit']) || isset($_SESSION['project'])) {
-		$project;
-		if (isset($_POST['project'])) {
-			$project = $_POST['project'];
-			$_SESSION['project'] = $project;
-		}
-		else if (isset($_SESSION['project'])) {
-			$project = $_SESSION['project'];
+				
+				
+
+	<?php
+
+	if (isset($_POST['iterationSubmit'])) {
+		$project = $_POST['project'];
+		$iteration = $_POST['iteration'];
+
+
+		if ($iteration == "none" || $project == "none") {
+			echo "<br>invalid input.<br>";
+			exit();
 		}
 
-		//Get all iterations of the project
-		$query = "MATCH (p:Project {name: '$project'})
-					<-[:IS_ITERATION_OF]-(i:Iteration) 
-					RETURN i.name as iteration";
+		$outputList = "<ul>";
+
+		$query = "MATCH (p:Project {name: '$project'}) 
+					MATCH (i:Iteration {name: '$iteration'})-[:IS_ITERATION_OF]->(p)
+					MATCH (i)<-[:BELONGS_TO]-(files)-[:IS_INCLUDED_IN|:IS_REQUIRED_IN|:IS_USED_BY|:IMPACTS|:DECLARES*0..]->(feature:Feature)
+					WITH feature
+					ORDER BY feature.name ASC 
+					RETURN DISTINCT feature.name AS feature";
+
 		$result = runQuery($client, $query);
 
-		$iterationOptions = "";
-
 		foreach ($result->records() as $record) {
-			$iteration = $record->value('iteration');
-			$iterationOptions.= "<option value='$iteration'";
-			if (isset($_POST['iteration'])
-				&& $_POST['iteration'] == $iteration) {
-				$iterationOptions.= " selected";
-			}
-			$iterationOptions.= ">$iteration</option>";
+			$outputList.= "<li>".$record->value('feature')."</li>";
 		}
+		$outputList.= "</ul>";
 
-		?>
-
-		<div class="row">
-			<form class="col-lg-8 col-lg-offset-2 form-horizontal" 
-					method="post" action="index.php?features">
-				
-				<div class="row form-group">
-					<label>Select the iteration</label>
-					<select name="iteration">
-						<?php
-							echo $iterationOptions;
-						?>
-					</select>
-					<button class="btn btn-primary" type="submit" 
-							name="iterationSubmit">Validate
-					</button>
-				</div>
-
-			</form>
-		</div>
-
-		<?php
-
-		if (isset($_POST['iterationSubmit'])) {
-			$project = $_SESSION['project'];
-			$iteration = $_POST['iteration'];
-
-			$outputList = "<ul>";
-
-			$query = "MATCH (p:Project {name: '$project'}) 
-						MATCH (i:Iteration {name: '$iteration'})-[:IS_ITERATION_OF]->(p)
-						MATCH (i)<-[:BELONGS_TO]-(files)-[:IS_INCLUDED_IN|:IS_REQUIRED_IN|:IS_USED_BY|:IMPACTS|:DECLARES*0..]->(feature:Feature)
-						WITH feature
-						ORDER BY feature.name ASC 
-						RETURN DISTINCT feature.name AS feature";
-
-			$result = runQuery($client, $query);
-
-			foreach ($result->records() as $record) {
-				$outputList.= "<li>".$record->value('feature')."</li>";
-			}
-			$outputList.= "</ul>";
-			echo $outputList;
-		}
-
+		echo "<h2>$project -> $iteration</h2><br>";
+		echo $outputList;
 	}
 ?>
--- functions/database_functions.php
+++ functions/database_functions.php
@@ -3,14 +3,13 @@
 		§§ Request Database
 	*/
 
-	include "functions/display_exceptions_functions.php";
+	include __DIR__."/display_exceptions_functions.php";
 
 /**
 	Allows to run a query to a connected neo4j database, and catch potential Exceptions
 	@param client is the link to the database
 	@param query is the query to send
-	@return is the result of the query, of false is an Exception is thrown while the 
-		query was sent to database
+	@return is the result of the query, of false is an Exception is thrown while the query was sent to database
 */
 function runQuery($client, $query) {
 	try {
